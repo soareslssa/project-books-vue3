35a11fb270a81a965068b2c1c21099a3b4fa614b
soareslss <soareslssa@fab.mil.br>
2023-10-09 14:52:01 -0300

    feat: implement Book component

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,62 @@
 <script setup>
-import { reactive } from 'vue';
-import Progress from './components/Progress.vue'
+import Books from "./components/Books.vue"
 
-let newCourse = {done:false};
-
-let courses = reactive([
-  {
-    title: 'Javascript',
-    done: true
-  },  
-  {
-    title: 'React',
-    done: false
-  },  
-  {
-    title: 'Vue',
-    done: false
-  },
-])
-
-function addCourse () {
-  courses.push(newCourse);
-  newCourse = {done:false}
-}
-
+let books =     [
+            {
+              id: 1,
+              title: "History of Europe",
+              cover:
+                "https://printpress.cmsmasters.net/default/wp-content/uploads/sites/11/2019/05/printpress-product-6-540x861.jpg",
+              isRead: true,
+              isbn: "0-395-07157-8",
+              author: "Daniel Trejo",
+            },
+            {
+              id: 2,
+              title: "Penguin Classics",
+              cover:
+                "https://printpress.cmsmasters.net/default/wp-content/uploads/sites/11/2019/05/printpress-product-2-540x861.jpg",
+              isRead: false,
+              isbn: "0-395-07157-8",
+              author: "Daniel Trejo, Jon Snow",
+            },
+            {
+              id: 3,
+              title: "Becoming",
+              cover:
+                "https://printpress.cmsmasters.net/default/wp-content/uploads/sites/11/2019/05/printpress-product-7-540x861.jpg",
+              isRead: false,
+              isbn: "0-395-07157-8",
+              author: "Daniel Trejo",
+            },
+            {
+              id: 4,
+              title: "Sonnets",
+              cover:
+                "https://printpress.cmsmasters.net/default/wp-content/uploads/sites/11/2019/05/printpress-product-5-540x861.jpg",
+              isRead: false,
+              isbn: "0-395-07157-8",
+              author: "Daniel Trejo",
+            },
+          ]
 
 </script>
 <template>
-  <div>
-      <h1>Lista de Cursos</h1>
-      <h2 v-for="(course, index) in courses" :key="index">
-          {{ course.title }}
-      </h2>
-
-      <br/>
-      <input v-model="newCourse.title" type="text">
-      <button @click="addCourse">Adicionar</button>
-    </div>
-    <Progress :courses="courses"/>
+    <div class="container">
+        <h1>📖 Meus Livros</h1>
+        <div class="header-btns">
+          <button
+            class="btn"
+            
+          >
+            Adicionar Livro +
+          </button>
+        </div>
+     
+        <div class="books-container">
+          <Books :books="books"/>
+        </div>
+      </div>
   </template>  
 
 <style scoped>
