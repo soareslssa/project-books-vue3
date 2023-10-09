1d7a8bb3a735d18a4c83c48269688af7284ce8a1
soareslss <soareslssa@fab.mil.br>
2023-10-09 10:54:19 -0300

    feat: implement course list reactive

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,29 @@
 <script setup>
-import { ref } from 'vue';
+import { ref ,reactive } from 'vue';
 
 // ref para primitivos: valores ficam armazenados dentro do value
 let count = ref(0);
+
+// reactive para objetos
+
+let newCourse = {};
+
+let courses = reactive([
+  {
+    title: 'Javascript'
+  },  
+  {
+    title: 'React'
+  },  
+  {
+    title: 'Vue'
+  },
+])
+
+function addCourse () {
+  courses.push(newCourse);
+  newCourse = {}
+}
 
 function increment() {
   count.value++;
@@ -14,6 +35,15 @@ function increment() {
   <div>
       <h1>{{ count }}</h1>
       <button @click="increment">Incrementar</button>
+
+      <h1>Lista de Cursos</h1>
+      <h2 v-for="(course, index) in courses" :key="index">
+          {{ course.title }}
+      </h2>
+
+      <br/>
+      <input v-model="newCourse.title" type="text">
+      <button @click="addCourse">Adicionar</button>
   </div>
 </template>
 
