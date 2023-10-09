21b3648519b8fc1aa6d9e5d89f3a1d1817f6bd41
soareslss <soareslssa@fab.mil.br>
2023-10-09 10:43:11 -0300

    feat: increment count with ref

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <script setup>
-</script>
+import { ref } from 'vue';
 
+// ref para primitivos: valores ficam armazenados dentro do value
+let count = ref(0);
+
+function increment() {
+  count.value++;
+}
+
+
+</script>
 <template>
   <div>
-
+      <h1>{{ count }}</h1>
+      <button @click="increment">Incrementar</button>
   </div>
 </template>
 
